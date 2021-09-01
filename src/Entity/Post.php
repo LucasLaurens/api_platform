f0e10947945b213a1b20c8bbcc60eaab00070888
LucasLaurens <lucaslaurens.74@gmail.com>
2021-09-01 16:58:54 +0200

    add normalization and denormalization on the api

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -4,11 +4,23 @@ namespace App\Entity;
 
 use App\Repository\PostRepository;
 use ApiPlatform\Core\Annotation\ApiResource;
+use DateTime;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=PostRepository::class)
- * @ApiResource()
+ * @ApiResource(
+ *       normalizationContext={"groups"={"read:collection"}},
+ *       denormalizationContext={"groups"={"write:Post"}},
+ *       itemOperations={
+ *         "put",
+ *         "delete",
+ *         "get"={
+ *              "normalization_context"={"groups"={"read:collection", "read:item", "read:Post"}}
+ *          }
+ *     }
+ * )
  */
 class Post
 {
@@ -16,26 +28,31 @@ class Post
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"read:collection"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"read:collection", "write:Post"})
      */
     private $title;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"read:collection", "write:Post"})
      */
     private $slug;
 
     /**
      * @ORM\Column(type="text")
+     * @Groups({"read:item", "write:Post"})
      */
     private $content;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Groups({"read:item"})
      */
     private $createdAt;
 
@@ -46,8 +63,15 @@ class Post
 
     /**
      * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="posts")
+     * @Groups({"read:item", "write:Post"})
      */
     private $category;
+
+    public function __construct()
+    {
+        $this->createdAt = new DateTime();
+        $this->updatedAt = new DateTime();
+    }
 
     public function getId(): ?int
     {
